cf41844b36237dcb54b2c45d378050ca01c1f44a
Raynard Brown <brown_raynard@icloud.com>
2024-08-20 15:44:35 -0500

    Iteration1

---
+++ IterationStatementsExercise1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace IterationStatementsExercise1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 7; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 8; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        
+
+        
+        
+    }
+    
+}
